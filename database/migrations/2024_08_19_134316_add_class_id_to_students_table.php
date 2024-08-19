d1988b8acbb4b3feb37d5c1de8c53563d56e62c5
adnanhyderluqon <12345adnan@gmail.com>
2024-08-19 20:14:51 +0500

    table updated logic like if class deleted no effect on student

--- database/migrations/2024_08_19_134316_add_class_id_to_students_table.php
+++ database/migrations/2024_08_19_134316_add_class_id_to_students_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table(school_prefix().'students', function (Blueprint $table) {
-            $table->foreignId('class_id')->nullable()->constrained(school_prefix().'classes')->onDelete('cascade');        });
+            $table->integer('class_id')->nullable();
+        });
     }
 
     /**
@@ -21,7 +22,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table(school_prefix().'school_user', function (Blueprint $table) {
-            $table->dropForeign(['class_id']);
             $table->dropColumn('class_id');
         });
     }
